85b2775c6464e1743fac440cc16fb331a431d33b
Rafael <rafaelpianaro@gmail.com>
2022-02-20 20:57:38 -0300

    *menu-items *menu-logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,18 @@
 <template>
   <header>
       <nav class="container">
-            <a href=""><img id="logo" src="https://raw.githubusercontent.com/rafaelpianaro/mock-site-vue-axios-resources/main/assets/images/wdev.svg" alt="logo"></a>
+            <a href="#"><img id="logo" src="https://raw.githubusercontent.com/rafaelpianaro/mock-site-vue-axios-resources/main/assets/images/wdev.svg" alt="logo"></a>
             <img id="menu-button" src="https://raw.githubusercontent.com/rafaelpianaro/mock-site-vue-axios-resources/main/assets/images/menu.svg" alt="Abrir menu">
             <div id="menu-overlay"></div>
+            <div id="menu-items">
+                <img id="menu-logo" src="https://raw.githubusercontent.com/rafaelpianaro/mock-site-vue-axios-resources/main/assets/images/wdev.svg" alt="logo">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/videos">Vídeos</a></li>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </div>
       </nav>
   </header>
 </template>
@@ -46,6 +55,38 @@ export default {
         height: 100vh;
         background-color: #000;
         opacity: 0.8;
+    }
+
+    #menu-logo {
+        width: 110px;
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+
+    #menu-items {
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: var(--color-background-nav);
+        width: 60%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    ul {
+        list-style: none;
+        text-align: center;
+    }
+
+    ul li {
+        margin: 20px 0;
+    }
+
+    ul li a {
+        color: var(--color-text-light);
     }
 
 </style>
