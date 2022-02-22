70f70ac39543137ecf63bb39c39b55847f6a4970
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-02-22 12:56:05 -0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <Header />
     <Home />
+    <Footer />
   </div>
 </template>
 
 <script>
 import './styles/global.css'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 import Home from './pages/Home.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Home
+    Home,
+    Footer
   }
 }
 </script>
