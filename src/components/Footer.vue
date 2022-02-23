e41ea0f9b1075a732855571b20f212443be5e59f
Rafael <rafaelpianaro@gmail.com>
2022-02-22 22:01:41 -0300

    *footer *get infos from api

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <footer>
-
+      <a class="social-link" v-for="(socialLink, index) in socialLinks" :key="index" href="socialLink.link" target="_blank">
+          <img :alt="socialLink.label" :title="socialLink.label" :src="socialLink.icon">
+      </a>
   </footer>
 </template>
 
@@ -9,9 +11,15 @@ import api from '@/services/api.js'
 
 export default {
     name: 'Footer',
+    data(){
+        return {
+            socialLinks: []
+        }
+    },
     mounted() {
         api.get('/social-links.json').then(response => {
-            console.log(response.data)
+            // console.log(response.data)
+            this.socialLinks = response.data
         })
     }
 }
@@ -25,5 +33,20 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .social-link {
+        border: 2px solid var(--color-text-light);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin: 0 10px;
+    }
+
+    .social-link img {
+        width: 20px;
     }
 </style>
