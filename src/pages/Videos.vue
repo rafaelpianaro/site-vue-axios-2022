109f55628dc797cb0cb50589970d52d48ac39785
Rafael <rafaelpianaro@gmail.com>
2022-02-27 19:59:25 -0300

    added page videos

---
+++ src/pages/Videos.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+      <section class="container">
+          <h1>Vídeos</h1>
+          <section class="videos">
+              <a href="" class="vide">
+                  <img src="" alt="">
+                  <div class="videos-title">
+                      
+                  </div>
+              </a>
+          </section>
+      </section>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Videos'
+
+}
+</script>
+
+<style scoped>
+ main {
+     align-items: center;
+ }
+</style>
