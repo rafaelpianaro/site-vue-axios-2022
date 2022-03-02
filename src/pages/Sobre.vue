49c8dae3ebcc120457f189d691132278e064637c
Rafael <rafaelpianaro@gmail.com>
2022-03-01 21:11:34 -0300

    added about

---
+++ src/pages/Sobre.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+
+      <section class="container">
+
+          <h1>Sobre</h1>
+
+          <div class="about" v-html="about"></div>
+
+      </section>
+
+  </main>
+</template>
+
+<script>
+import api from '@/services/api.js'
+export default {
+    name: 'Sobre',
+    data() {
+        return {
+            about: ''
+        }
+    },
+    mounted() {
+        api.get('/about.json').then(resonse => {
+            this.about = resonse.data.about
+        })
+    }
+
+}
+</script>
+
+<style scoped>
+
+ main {
+     align-items: center;
+ }
+
+ .about {
+     margin-bottom: 40px;
+     color: var(--color-text-dark);
+ }
+
+</style>
